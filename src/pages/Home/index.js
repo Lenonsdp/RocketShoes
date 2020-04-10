b1cc36ef042c126f6e57653b857b899697e923f0
Lenon <lenon.paula@bling.com.br>
2020-04-10 20:54:44 -0300

    Estilização de lista de produtos

---
+++ src/pages/Home/index.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import { MdShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './style';
+
+export default function Home() {
+  return (
+    <ProductList>
+      <li>
+          <img src='https://mizuno.vteximg.com.br/arquivos/ids/196236-1000-1000/4144307_0090_01.jpg?v=637093626348700000' alt='Tênis'/>
+            <strong>Tênis Nike</strong>
+            <span>R$ 129,99</span>
+
+            <button type='button'>
+              <div> 
+                <MdShoppingCart size='16' color='#fff' />
+              </div>
+
+              <span>Adicionar ao carrinho</span>
+            </button>
+      </li>
+
+      <li>
+          <img src='https://mizuno.vteximg.com.br/arquivos/ids/196236-1000-1000/4144307_0090_01.jpg?v=637093626348700000' alt='Tênis'/>
+            <strong>Tênis Nike</strong>
+            <span>R$ 129,99</span>
+
+            <button type='button'>
+              <div> 
+                <MdShoppingCart size='16' color='#fff' />
+              </div>
+
+              <span>Adicionar ao carrinho</span>
+            </button>
+      </li>
+
+      <li>
+          <img src='https://mizuno.vteximg.com.br/arquivos/ids/196236-1000-1000/4144307_0090_01.jpg?v=637093626348700000' alt='Tênis'/>
+            <strong>Tênis Nike</strong>
+            <span>R$ 129,99</span>
+
+            <button type='button'>
+              <div> 
+                <MdShoppingCart size='16' color='#fff' />
+              </div>
+
+              <span>Adicionar ao carrinho</span>
+            </button>
+      </li>
+
+      <li>
+          <img src='https://mizuno.vteximg.com.br/arquivos/ids/196236-1000-1000/4144307_0090_01.jpg?v=637093626348700000' alt='Tênis'/>
+            <strong>Tênis Nike</strong>
+            <span>R$ 129,99</span>
+
+            <button type='button'>
+              <div> 
+                <MdShoppingCart size='16' color='#fff' />
+              </div>
+
+              <span>Adicionar ao carrinho</span>
+            </button>
+      </li>
+
+      <li>
+          <img src='https://mizuno.vteximg.com.br/arquivos/ids/196236-1000-1000/4144307_0090_01.jpg?v=637093626348700000' alt='Tênis'/>
+            <strong>Tênis Nike</strong>
+            <span>R$ 129,99</span>
+
+            <button type='button'>
+              <div> 
+                <MdShoppingCart size='16' color='#fff' />
+              </div>
+
+              <span>Adicionar ao carrinho</span>
+            </button>
+      </li>
+
+      <li>
+          <img src='https://mizuno.vteximg.com.br/arquivos/ids/196236-1000-1000/4144307_0090_01.jpg?v=637093626348700000' alt='Tênis'/>
+            <strong>Tênis Nike</strong>
+            <span>R$ 129,99</span>
+
+            <button type='button'>
+              <div> 
+                <MdShoppingCart size='16' color='#fff' />
+              </div>
+
+              <span>Adicionar ao carrinho</span>
+            </button>
+      </li>
+    </ProductList> 
+  );
+}
