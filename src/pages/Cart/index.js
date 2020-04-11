95f2f193f85a5be4e4826ed8ff32a8461e8554e6
Lenon <lenon.paula@bling.com.br>
2020-04-11 13:19:00 -0300

    Estilização do carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,9 +1,59 @@
 import React from 'react';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './style';
 
 export default function Cart() {
   return (
-    <div></div>
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+          <tbody>
+			<tr>
+				<td>
+					<img src='https://mizuno.vteximg.com.br/arquivos/ids/196236-1000-1000/4144307_0090_01.jpg?v=637093626348700000' alt='Produto'/>              
+				</td>
+				<td>
+					<strong>Tênis</strong>
+					<span>R$ 129,90</span>
+				</td>
+				<td>
+					<div>
+						<button type='button'>
+							<MdRemoveCircleOutline size={20} color='#7159c1'/>
+							<input type='number' readOnly value={1} />
+							<MdAddCircleOutline size={20} color='#7159c1'/>
+						</button>
+					</div>
+				</td>
+				<td>
+					<strong>R$ 258,80</strong>
+				</td>
+				<td>
+				<button type='button'>
+					<MdDelete size={20} color='#7159c1' />
+				</button>
+				</td>
+			</tr>
+          </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type='button'>Finalizar pedido</button>
+
+        <Total>
+          <span>Total</span>
+          <strong>R$ 1950,00</strong>
+        </Total>
+      </footer>
+    </Container>
   );
 }
