4aaf6a57a45f2ef37b4d72bff41d867ab9423942
Lenon <lenon.paula@bling.com.br>
2020-04-21 17:38:37 -0300

    Adicionado react-toastfy e consulta de estoque;

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 import background from '../assets/images/back.svg';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 export default createGlobalStyle`
